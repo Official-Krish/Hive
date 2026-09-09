52bda8329f42ff1658835e29e3304736d2014f71
Official-Krish <krishanand0103@gmail.com>
2026-09-09 18:30:49 +0530

    [fix]: fixed wrong config

--- apps/frontend/src/lib/config.ts
+++ apps/frontend/src/lib/config.ts
@@ -1,7 +1,6 @@
-// export const API_BASE_URL = "https://api.hive.krishlabs.tech";
-// export const WS_BASE_URL = "wss://ws.hive.krishlabs.tech";
-// export const ASSET_BASE_URL = "https://cdn.krishlabs.tech/hive";
-
-export const API_BASE_URL = "http://localhost:3000";
-export const WS_BASE_URL = "ws://localhost:4001";
+export const API_BASE_URL = "https://api.hive.krishlabs.tech";
+export const WS_BASE_URL = "wss://ws.hive.krishlabs.tech";
 export const ASSET_BASE_URL = "https://cdn.krishlabs.tech/hive";
+
+// export const API_BASE_URL = "http://localhost:3000";
+// export const WS_BASE_URL = "ws://localhost:4001";
